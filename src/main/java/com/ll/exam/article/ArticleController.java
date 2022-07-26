6cdcf44923a6cb03e94cc37ebee4c9fa001e4daf
SG1515 <sunggu507@naver.com>
2022-07-26 15:35:04 +0900

    게시물 작성폼 구현

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -15,8 +15,13 @@ public class ArticleController {
         articleDtos.add(new ArticleDto(2, "제목 2", "내용 2"));
         articleDtos.add(new ArticleDto(1, "제목 1", "내용 1"));
 
-        rq.setAttr("articles", articleDtos);
 
+        rq.setAttr("articles", articleDtos);
         rq.view("usr/article/list");
+    }
+
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+
     }
 }
