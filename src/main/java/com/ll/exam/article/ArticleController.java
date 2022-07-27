6235fd8ae5487925876a744b5eb888d2c5bad0f1
SG1515 <sunggu507@naver.com>
2022-07-27 14:45:17 +0900

    ArticleService, ArticleRepository 도입

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -7,6 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleController {
+
+    private ArticleService articleService;
+
+    public ArticleController() {
+        articleService = new ArticleService();
+    }
+
     public void showList(Rq rq) {
         List<ArticleDto> articleDtos = new ArrayList<>();
         articleDtos.add(new ArticleDto(5, "제목 5", "내용 5"));
@@ -29,7 +36,8 @@ public class ArticleController {
         String title = rq.getParam("title", "");
         String body = rq.getParam("body", "");
 
-        rq.appendBody("<div>title : %s</div>".formatted(title));
-        rq.appendBody("<div>body : %s</div>".formatted(body));
+        long id = articleService.write(title, body);
+
+        rq.appendBody("%d번 게시물이 생성되었습니다.".formatted(id));
     }
 }
