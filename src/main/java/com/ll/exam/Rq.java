881149e732056824b13009c0a7b878cb3e08e2d2
SG1515 <sunggu507@naver.com>
2022-07-28 10:10:29 +0900

    getLongPathValueByIndex 도입, 상세페이지 적잘한 게시물 노출

--- src/main/java/com/ll/exam/Rq.java
+++ src/main/java/com/ll/exam/Rq.java
@@ -89,4 +89,29 @@ public class Rq {
         //공통/컨트롤러/액션
     }
 
+    public long getLongPathValueByIndex(int index, long defaultValue) {
+        String value = getPathValueByIndex(index, null);
+
+        if ( value == null ) {
+            return defaultValue;
+        }
+
+        try {
+            return Long.parseLong(value);
+        }
+        catch ( NumberFormatException e ) {
+            return defaultValue;
+        }
+    }
+
+    public String getPathValueByIndex(int index, String defaultValue) {
+        String[] bits = req.getRequestURI().split("/");
+
+        try {
+            return bits[4 + index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return defaultValue;
+        }
+    }
+
 }
